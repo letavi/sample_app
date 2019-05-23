7030df93a4af230a1cb3ad7b6632bce96fe39ee8
letavi <ltviet.nk@gmail.com>
2019-05-23 13:03:42 +0700

    Test email of user must be presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   # Validate name of user must be presence
   validates :name, presence: true
+
+  #  Validate email of user must be presence
+  validates :email, presence: true
 end
