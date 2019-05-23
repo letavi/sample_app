2d9ac2e326d595860ce2e1d949fb6cd00c67d551
letavi <ltviet.nk@gmail.com>
2019-05-23 13:27:32 +0700

    Add secure password for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false } 
+  
+  # Add secure password for user
+  has_secure_password
 end
