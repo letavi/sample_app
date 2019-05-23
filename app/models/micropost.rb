d1659b1135bae8b5887624dcf08bfd5bc72ce049
letavi <ltviet.nk@gmail.com>
2019-05-23 14:00:32 +0700

    Fix error validate

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,8 +3,8 @@ class Micropost < ApplicationRecord
   # Define association with user
   belongs_to :user
   # Validate for title
-  validates :title, prsence: true, length: { in: 5..100 }
+  validates :title, presence: true, length: { in: 5..100 }
   
   # Validate for content
-  validates :contant, prsence: true, length: { in: 5..1000 } 
+  validates :contant, presence: true, length: { in: 5..1000 } 
 end
