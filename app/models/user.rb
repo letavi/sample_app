3ac8b35a56afa30cd48dfb2c922b50b7acb4457b
letavi <ltviet.nk@gmail.com>
2019-05-23 13:09:07 +0700

    Test validations of format email. Add validate for format of email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+
+  # Format of email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   # Validate name of user must be presence, length of name
   validates :name, presence: true, length: { maximum: 50 }
 
   #  Validate email of user must be presence, length of email
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
 end
