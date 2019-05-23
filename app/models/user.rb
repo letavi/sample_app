8fc46b40427cb6ed5f96e18f21b192e2f258fb09
letavi <ltviet.nk@gmail.com>
2019-05-23 13:14:41 +0700

    Explain:
    - Some case, if user add their email with uppercase, we need config false for this case.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   #  Validate email of user must be presence, length of email
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+            uniqueness: { case_sensitive: false } 
 end
