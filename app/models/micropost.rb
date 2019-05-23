0eb84b3b8d23babe2d77375dd0e5507bff4e3ec2
letavi <ltviet.nk@gmail.com>
2019-05-23 13:57:03 +0700

    Validates for micropost model AND define association between user and micropost model.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,10 @@
 class Micropost < ApplicationRecord
+
+  # Define association with user
   belongs_to :user
+  # Validate for title
+  validates :title, prsence: true, length: { in: 5..100 }
+  
+  # Validate for content
+  validates :contant, prsence: true, length: { in: 5..1000 } 
 end
