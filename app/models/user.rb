2fde333b428b1dd4f434446f28d9288f706d6f9f
letavi <ltviet.nk@gmail.com>
2019-05-23 13:06:40 +0700

    Test length of name and email. Add validate for length of name and email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  # Validate name of user must be presence
-  validates :name, presence: true
+  # Validate name of user must be presence, length of name
+  validates :name, presence: true, length: { maximum: 50 }
 
-  #  Validate email of user must be presence
-  validates :email, presence: true
+  #  Validate email of user must be presence, length of email
+  validates :email, presence: true, length: { maximum: 255 }
 end
